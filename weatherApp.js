0582fc1445c78da2bd3e2834a422169541478135
norraynor <alechanderr@gmail.com>
2021-08-29 12:00:30 +0200

    functioning prototype

--- weatherApp.js
+++ weatherApp.js
@@ -7,20 +7,36 @@ const input = document.querySelector("input");
 const tempPara = document.querySelector("#temp");
 const weatherPara = document.querySelector("#weather");
 const header = document.querySelector("#weather-message");
-
+let timePassed = 0;
+const time = document.querySelector('#time');
+const tempUnitsInput = document.querySelectorAll('.tempSelector');
+const tempUnitsArr = [...tempUnitsInput];
 
 button.addEventListener("click",()=>{
-    getAPI(input.value);
+    let tempValue ="";
+    tempUnitsArr.forEach(element => {
+        if(element.checked)
+        tempValue=element.value;
+    });
+    var start = window.performance.now();
+ 
+    // task starts
+    getAPI(input.value,tempValue);
+    // task ends
+    
+    var end = window.performance.now();
+    timePassed = end - start;
+    time.textContent = `Data retrieved in: ${timePassed} ms`;
 })
 
-async function getAPI(location){
+async function getAPI(location,tempUnit){    
     const response = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${location}&APPID=1befe19e51fbd156de9827eb7086ec09`);
     const responseData = await response.json()
-    console.log(responseData);
+    /*console.log(responseData);
     console.log(responseData.name)
     console.log(getTemperature(responseData.main,C).getFeelsTemp())
-    console.log(responseData.weather[0].icon);
-    tempPara.textContent = `temperature is: ${getTemperature(responseData.main,C).getFeelsTemp()}`;
+    console.log(responseData.weather[0].icon);*/
+    tempPara.textContent = `temperature is: ${getTemperature(responseData.main,tempUnit).getFeelsTemp()} ${tempUnit}`;
     getWeatherIcon(responseData.weather[0].icon);
     weatherPara.textContent = `current weather: ${responseData.weather[0].description}`
     header.textContent = `Current weather for ${responseData.name} `;
@@ -28,18 +44,28 @@ async function getAPI(location){
 
 function getTemperature(responseData,tempUnits){
     const KELVIN = 273.15;
+    switch(tempUnits){
+        case "C":
+            tempUnits = C;
+            break;
+        case "F":
+            tempUnits = F;
+            break;
+    }
     function getCelcius(temp){
         return temp - KELVIN;
     }
     function getFahrenheit(temp){
         return getCelcius(temp)* 9/5 + 32;
     }
+
     function getCurrentTemp(){
         return getTemp(responseData.temp).toFixed(2);
     }
     function getFeelsTemp(){
         return getTemp(responseData.feels_like).toFixed(2);
     }
+
     function getTemp(temp){
         switch(tempUnits){
             case "KELVIN":
