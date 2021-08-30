cd63217dfc9c41bbf07d88fed395f0dcc9c0f9e7
norraynor <alechanderr@gmail.com>
2021-08-30 22:30:34 +0200

    added error handling and visual changes

--- weatherApp.js
+++ weatherApp.js
@@ -11,6 +11,7 @@ let timePassed = 0;
 const time = document.querySelector('#time');
 const tempUnitsInput = document.querySelectorAll('.tempSelector');
 const tempUnitsArr = [...tempUnitsInput];
+const resultDiv = document.querySelector('#result');
 
 button.addEventListener("click",()=>{
     let tempValue ="";
@@ -30,16 +31,21 @@ button.addEventListener("click",()=>{
 })
 
 async function getAPI(location,tempUnit){    
-    const response = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${location}&APPID=1befe19e51fbd156de9827eb7086ec09`);
-    const responseData = await response.json()
-    /*console.log(responseData);
-    console.log(responseData.name)
-    console.log(getTemperature(responseData.main,C).getFeelsTemp())
-    console.log(responseData.weather[0].icon);*/
-    tempPara.textContent = `temperature is: ${getTemperature(responseData.main,tempUnit).getFeelsTemp()} ${tempUnit}`;
-    getWeatherIcon(responseData.weather[0].icon);
-    weatherPara.textContent = `current weather: ${responseData.weather[0].description}`
-    header.textContent = `Current weather for ${responseData.name} `;
+    try {
+        const response = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${location}&APPID=1befe19e51fbd156de9827eb7086ec09`);    
+        if(!response.ok){
+            throw new Error(response.statusText);
+        }  
+        const responseData = await response.json();
+        tempPara.textContent = `temperature is: ${getTemperature(responseData.main,tempUnit).getFeelsTemp()} ${tempUnit}`;
+        getWeatherIcon(responseData.weather[0].icon);
+        weatherPara.textContent = `current weather: ${responseData.weather[0].description}`
+        header.textContent = `Current weather for ${responseData.name} `;
+        resultDiv.classList.remove('hidden'); 
+             
+    } catch (error) {
+        header.textContent = `City ${error.message}`;
+    }
 }
 
 function getTemperature(responseData,tempUnits){
